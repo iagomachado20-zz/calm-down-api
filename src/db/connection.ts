39e55daa743696a0f5760e08a40831df573d2469
Iago Machado <iago.machado@S2IT.CORP>
2020-05-02 15:17:54 -0300

    Update databasename

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,3 +1,3 @@
 export const DataBase = {
-    connectionString: 'mongodb+srv://calmdown:calmdown1234@cluster0-0qhz4.mongodb.net/test?retryWrites=true&w=majority'
+    connectionString: 'mongodb+srv://calmdown:calmdown1234@cluster0-0qhz4.mongodb.net/calmdown?retryWrites=true&w=majority'
 };
